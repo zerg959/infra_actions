3bcdfe271e491916cd3a3c9b8a198d19e34d4992
Ilya Lukichev <zerg959@yandex.ru>
2022-07-28 15:20:59 +0300

    add test running

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -9,10 +9,10 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('')
+        response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
@@ -20,5 +20,5 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
